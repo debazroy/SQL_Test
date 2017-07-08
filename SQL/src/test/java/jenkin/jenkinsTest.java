babbacfd769c2bb907d249cafd6cbaa7da64ea4e
debazroy <debazroy@yahoo.com>
2017-07-08 17:10:57 -0400

    new package jenkin created and new class jenkinsTest created to test online.city.com tile

---
+++ SQL/src/test/java/jenkin/jenkinsTest.java
@@ -0,0 +1,27 @@
+package jenkin;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import junit.framework.Assert;
+
+public class jenkinsTest {
+	WebDriver driver = null;
+  @Test
+  public void jk() {
+	  ChromeDriverManager.getInstance().setup();
+	  driver = new ChromeDriver();
+	  driver.get("https://online.citi.com/US/login.do");
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	  String ActualTitle = driver.getTitle();
+	  String ExpectedTitle = "Banking with Citi | Citi.com";
+	  Assert.assertEquals(ActualTitle, ExpectedTitle);
+	  System.out.println(ActualTitle);
+	  driver.quit();
+  }
+}
