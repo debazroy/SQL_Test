0405fd3a921008ac89328f5e35b56366282e0106
debazroy <debazroy@yahoo.com>
2017-07-12 02:00:12 -0400

    25th line colon removed

--- SQL/src/test/java/jenkin/jenkinsTest.java
+++ SQL/src/test/java/jenkin/jenkinsTest.java
@@ -22,6 +22,6 @@ public class jenkinsTest {
 	  String ExpectedTitle = "Banking with Citi | Citi.com";
 	  Assert.assertEquals(ActualTitle, ExpectedTitle);
 	  System.out.println(ActualTitle);
-	  driver.quit();
+	  driver.quit()
   }
 }
